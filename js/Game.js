e7aafee151a20becba66bd6735075c8f9d64715e
Aakash1495 <61931750+Aakash1495@users.noreply.github.com>
2020-05-29 13:59:14 +0530

    wow

--- js/Game.js
+++ js/Game.js
@@ -30,9 +30,13 @@ class Game {
     }
 
     car1 = createSprite(100,200);
+    car1.addImage("player1", c1);
     car2 = createSprite(300,200);
+    car2.addImage("player2", c2);
     car3 = createSprite(500,200);
+    car3.addImage("player3", c3);
     car4 = createSprite(700,200);
+    car4.addImage("player4", c4);
     cars = [car1, car2, car3, car4];
   }
 
@@ -42,13 +46,15 @@ class Game {
     Player.getPlayerInfo();
     
     if(allPlayers !== undefined){
+      background(gd);
+      image(tk, 0, -displayHeight*4, displayWidth, displayHeight*5);
       //var display_position = 100;
       
       //index of the array
       var index = 0;
 
       //x and y position of the cars
-      var x = 0;
+      var x = 200;
       var y;
 
       for(var plr in allPlayers){
@@ -63,6 +69,8 @@ class Game {
         cars[index-1].y = y;
 
         if (index === player.index){
+          fill("blue");
+          ellipse(x,y,60,60);
           cars[index - 1].shapeColor = "red";
           camera.position.x = displayWidth/2;
           camera.position.y = cars[index-1].y
--- sketch.js
+++ sketch.js
@@ -8,8 +8,17 @@ var database;
 
 var form, player, game;
 
+var c1, c2, c3, c4, gd, tk;
 var cars, car1, car2, car3, car4;
 
+function preload(){
+c1 = loadImage("images/car1.png");
+c2 = loadImage("images/car2.png");
+c3 = loadImage("images/car3.png");
+c4 = loadImage("images/car4.png");
+gd = loadImage("images/ground.png");
+tk = loadImage("images/track.jpg");
+}
 
 function setup(){
   canvas = createCanvas(displayWidth - 20, displayHeight-30);
